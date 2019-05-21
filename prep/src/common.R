b78436c49704be6982ee0b6de4ca948cc3482e11
Marina Espinasse <ecologM@hotmail.com>
2019-05-21 13:38:59 +0200

    Small update to source code

--- prep/src/common.R
+++ prep/src/common.R
@@ -10,7 +10,7 @@ dir_M             <- c('Windows' = '//ftp.imr.no', #main directory to get to
 
 # WARN rather than stop if directory doesn't exist
 if (!file.exists(sprintf('%s/',dir_M))){
-  warning(sprintf("The directory for variable dir_M set in src/R/common.R does not exist. Do you need to mount %s?", dir_M))
+  warning(sprintf("The directory for variable dir_M set in src/common.R does not exist. Do you need to mount %s?", dir_M))
   
 }
 
@@ -33,3 +33,5 @@ library(rgdal)
 library(raster)
 
 rm(packages)
+
+select <- dplyr::select
